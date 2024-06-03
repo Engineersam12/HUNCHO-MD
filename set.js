dd466436ba05277c7a5ab198a68aa4a8c73fc60f
Engineersam12 <engineermuema750@gmail.com>
2024-06-03 21:31:21 +0300

    Update set.js

--- set.js
+++ set.js
@@ -9,7 +9,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || "+",
-    OWNER_NAME: process.env.OWNER_NAME || "Keith Keizzah",
+    OWNER_NAME: process.env.OWNER_NAME || "Samuel muema",
     NUMERO_OWNER : process.env.OWNER_NUMBER || "",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
